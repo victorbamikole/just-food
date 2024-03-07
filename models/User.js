488631917e9596f3d7791314a086095c21b6f737
victorbamikole <victorbamikole92@gmail.com>
2024-03-07 13:30:47 -0800

    user model created

---
+++ models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const UaserSchema = new mongoose.Schema(
+  {
+    userName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    uid: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    address: { type: Array, required: false },
+    phone: { type: String, required: false },
+    userType: {
+      type: String,
+      required: true,
+      default: "Client",
+      enum: ["Admin", "Driver", "Client", "Vendor"],
+    },
+    profile: {
+      type: String,
+      required: true,
+      default:
+        "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358_1280.jpg",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UaserSchema);
--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import admin from "firebase-admin";
+import serviceAccount from "./servicesAccountKey.json" assert { type: "json" };
 
 const app = express();
-// require('dotenv').config();
 
 const PORT = 3000;
+dotenv.config();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
+
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-app.listen(PORT, () => {
-  console.log(`Server Running on http://localhost:${PORT}`);
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server Running on http://localhost:${process.env.PORT}`);
 });
